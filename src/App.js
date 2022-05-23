cf18b18290afafd81347d14dbd60b73ff4c9a2d8
Jalen Atkinson <Jalenatkinson35@gmail.com>
2022-05-22 21:37:24 -0400

    deploying app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import portfolioImage from './img/space.jpg';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Jalen Atkinson Portfolio</h1>
+      <h2>Hello! Nice to meet you!</h2>
+        <img src={portfolioImage} alt="portfolio image1"/>
     </div>
   );
 }
